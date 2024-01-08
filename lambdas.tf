06d7e0393f05cc53f9b6c3cceb59418367ee8249
harinik05 <hka747910@gmail.com>
2024-01-08 00:19:30 -0500

    workflow commit actions

--- lambdas.tf
+++ lambdas.tf
@@ -6,7 +6,7 @@ output_path = "./python/hello-python.zip"
 }
 
 
-data "archive_file_node" "zip_the_js_code" {
+data "archive_file" "zip_the_js_code" {
 type        = "zip"
 source_dir  = "./nodejs/"
 output_path = "./nodejs/zipped_folder.zip"
@@ -20,12 +20,12 @@ handler                        = "index.lambda_handler"
 runtime                        = "python3.8"
 }
 
-resource "aws_lambda_function2" "terraform_lambda_func2" {
+resource "aws_lambda_function" "terraform_lambda_func_second" {
 filename                       = "./nodejs/zipped_folder.zip"
 function_name                  = "BucketAndDBCounter"
 role                           = "arn:aws:iam::720479364235:role/service-role/mylambdarole"
 handler                        = "index.handler"
 runtime                        = "nodejs14.x" 
-source_code_hash = filebase64("./nodejs/zipped_folder.zip")  # Use filebase64 function to calculate hash
+//source_code_hash = filebase64("./nodejs/zipped_folder.zip")  # Use filebase64 function to calculate hash
 
 }
--- main.tf
+++ main.tf
@@ -1,7 +1,32 @@
-provider "aws"{
+/*provider "aws"{
     access_key = "${var.aws_access_key}"
     secret_key = "${var.aws_secret_key}"
     region = "${var.region}"
+}*/
+
+terraform {
+ required_providers {
+   aws = {
+     source = "hashicorp/aws"
+   }
+ }
+ 
+ backend "s3" {
+   region = "${var.region}"
+   key    = "terraform.tfstate"
+ }
+}
+ 
+provider "aws" {
+ region = "${var.region}"
+}
+ 
+resource "aws_instance" "test_instance" {
+ ami           = "ami-830c94e3"
+ instance_type = "t2.nano"
+ tags = {
+   Name = "test_instance"
+ }
 }
 
 
