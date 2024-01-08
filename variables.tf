56c43260b872890762f339c7fed6ad3419d6036b
harinik05 <hka747910@gmail.com>
2024-01-08 00:20:41 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "bucket_name"{
+    default = "waterquality10989760"
+}
+
+variable "acl_value"{
+    default = "private"
+}
+
+
+
+variable "region"{
+    default = "us-east-1"
+}
+
+variable "target_bucket_name"{
+    default = "targetbucket-waterquality"
+}
+
+variable "destination_bucket_name" {
+  default     = "destination-bucket-123490"
+}
+
+variable "kms_key_id"{
+    default = "c3955164-5cde-42ce-95ac-cf9fda0cbcc4"
+
+}
