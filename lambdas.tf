49e6c8f15969aa03492b93c51f720b1311714412
Harini <63025647+harinik05@users.noreply.github.com>
2024-02-24 18:32:58 -0500

    Update lambdas.tf

--- lambdas.tf
+++ lambdas.tf
@@ -15,7 +15,7 @@ output_path = "./nodejs/zipped_folder.zip"
 resource "aws_lambda_function" "terraform_lambda_func" {
 filename                       = "./python/hello-python.zip"
 function_name                  = "Spacelift_Test_Lambda_Function"
-role                           = "arn:aws:iam::720479364235:role/service-role/mylambdarole"
+role                           = "arn:aws:iam::accountnum:role/service-role/mylambdarole"
 handler                        = "index.lambda_handler"
 runtime                        = "python3.8"
 }
@@ -23,7 +23,7 @@ runtime                        = "python3.8"
 resource "aws_lambda_function" "terraform_lambda_func_second" {
 filename                       = "./nodejs/zipped_folder.zip"
 function_name                  = "BucketAndDBCounter"
-role                           = "arn:aws:iam::720479364235:role/service-role/mylambdarole"
+role                           = "arn:aws:iam::accountnum:role/service-role/mylambdarole"
 handler                        = "index.handler"
 runtime                        = "nodejs14.x" 
 //source_code_hash = filebase64("./nodejs/zipped_folder.zip")  # Use filebase64 function to calculate hash
